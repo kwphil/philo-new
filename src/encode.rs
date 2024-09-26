a0792b7196b85af3764feea328591fee15fb6f0c
kwphil <121273949+kwphil@users.noreply.github.com>
2024-09-26 13:25:31 -0500

    Convert vecs to arr

--- src/encode.rs
+++ src/encode.rs
@@ -8,20 +8,20 @@ use crate::statement::*;
 static variables: HashMap<String, VariableDeclaration> = HashMap::new();
 static used_reg: Vec<String> = Vec::new();
 
-const registers64: Vec<&str> = vec![
+const registers64: [String] = {
     "rax", "rbx", "rcx", "rdx", "rsi", "rdi", /* rsp and rbp won't be used */
     "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"
-];
+};
 
-const registers32: Vec<&str> = vec![
+const registers32: [String] = {
     "eax", "ebx", "ecx", "edx", "esi", "edi",
     "r8d", "r9d", "r10d", "r11d", "r12d", "r13d", "r14d", "r15d"
-];
+};
 
-const registers8: Vec<&str> = vec![
+const registers8: [String] = {
     "al", "ah", "bl", "bh", "ch", "cl", "dh", "dl",
     "sil", "dil", "r8b", "r9b", "r10b", "r11b", "r12b", "r13b", "r14b", "r15b"
-];
+};
 
 pub fn binary_operation(ref left: &Box<Expression>, ref operator: &str, ref right: &Box<Expression>) -> String {
     let left_out = expression(left);
