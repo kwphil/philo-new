45996a87e0722a575fd0fc3bab3204e18cf00892
kwphil <121273949+kwphil@users.noreply.github.com>
2024-09-26 13:32:51 -0500

    fixed sized

--- src/encode.rs
+++ src/encode.rs
@@ -9,14 +9,14 @@ use crate::statement::*;
 static variables: LazyLock<HashMap<String, VariableDeclaration>> = LazyLock::new(|| HashMap::new());
 static used_reg: LazyLock<Vec<String>> = LazyLock::new(Vec::new());
 
-const registers64: [&str; 14] = [
+const registers64: [&str; 18] = [
     "rax", "rbx", "rcx", "rdx", "rsi", "rdi", /* rsp and rbp won't be used */
-    "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"
+    "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "", "", "", ""
 ];
 
-const registers32: [&str; 14] = [
+const registers32: [&str; 18] = [
     "eax", "ebx", "ecx", "edx", "esi", "edi",
-    "r8d", "r9d", "r10d", "r11d", "r12d", "r13d", "r14d", "r15d"
+    "r8d", "r9d", "r10d", "r11d", "r12d", "r13d", "r14d", "r15d", "", "", "", ""
 ];
 
 const registers8: [&str; 18] = [
